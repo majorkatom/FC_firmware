f4252a5564602932240b87ca5ccf71b1111d558f
majorkatom <77786775+majorkatom@users.noreply.github.com>
2022-11-30 02:28:18 +0100

    Remove unnecessary bit setting

--- Core/Inc/magneto.h
+++ Core/Inc/magneto.h
@@ -14,7 +14,7 @@
 #define MAG_RESET_REG_VAL (uint8_t)0b00100011
 #define MAG_CFG_REG_A_INIT_VAL (uint8_t)0b10000011
 #define MAG_CFG_REG_A_MEASURE_VAL (uint8_t)0b10000001
-#define MAG_CFG_REG_B_VAL (uint8_t)0b00000011
+#define MAG_CFG_REG_B_VAL (uint8_t)0b00000001
 #define MAG_CFG_REG_C_VAL (uint8_t)0b00110001
 
 typedef struct MAG_DataType_ {
